6cdc61c2995518afd1420370eb0ca9cca045f7cd
SigmaZardan <bibekbhujel077@gmail.com>
2024-10-18 06:54:50 +0545

    Initial Commit

---
+++ SwiftDay7Basics/SwiftDay7BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDay7BasicsApp.swift
+//  SwiftDay7Basics
+//
+//  Created by Bibek Bhujel on 18/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDay7BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftDay7BasicsTests/SwiftDay7BasicsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDay7BasicsTests.swift
+//  SwiftDay7BasicsTests
+//
+//  Created by Bibek Bhujel on 18/10/2024.
+//
+
+import Testing
+@testable import SwiftDay7Basics
+
+struct SwiftDay7BasicsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
