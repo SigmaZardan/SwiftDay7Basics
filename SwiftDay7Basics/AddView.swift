e40a0ef8a1971bf72e491ccf311c00e6aa554199
SigmaZardan <bibekbhujel077@gmail.com>
2024-10-20 14:58:27 +0545

    finished

---
+++ SwiftDay7Basics/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  SwiftDay7Basics
+//
+//  Created by Bibek Bhujel on 18/10/2024.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
